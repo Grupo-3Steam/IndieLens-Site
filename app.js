d80769d90d4b8656205ec3e40752b296d89115ff
VMBB-SPTECH <victor.bertolino@sptech.school>
2026-09-08 01:01:06 -0300

    refactor: alterando display do app ao executar

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-// var ambiente_processo = 'producao';
+//var ambiente_processo = 'producao';
 var ambiente_processo = 'desenvolvimento';
 
 var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
-// Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
-// A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
-
 require("dotenv").config({ path: caminho_env });
 
 var express = require("express");
@@ -15,7 +12,7 @@ var HOST_APP = process.env.APP_HOST;
 
 var app = express();
 
-//var indexRouter = require("./src/routes/index");
+// var indexRouter = require("./src/routes/index");
 //var usuarioRouter = require("./src/routes/usuarios");
 
 app.use(express.json());
@@ -29,17 +26,12 @@ app.use(cors());
 
 app.listen(PORTA_APP, function () {
     console.log(`
-    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
-    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
-    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
-    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
-    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
-    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
-    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
-    \n\n\n                                                                                                 
-    Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
-    Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
-    \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
-    \tSe .:producao:. você está se conectando ao banco remoto. \n\n
-    \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
+    ██╗███╗   ██╗██████╗ ██╗███████╗██╗     ███████╗███╗   ██╗███████╗
+    ██║████╗  ██║██╔══██╗██║██╔════╝██║     ██╔════╝████╗  ██║██╔════╝
+    ██║██╔██╗ ██║██║  ██║██║█████╗  ██║     █████╗  ██╔██╗ ██║███████╗
+    ██║██║╚██╗██║██║  ██║██║██╔══╝  ██║     ██╔══╝  ██║╚██╗██║╚════██║
+    ██║██║ ╚████║██████╔╝██║███████╗███████╗███████╗██║ ╚████║███████║
+    ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═══╝╚══════╝                                                                                               
+    Para acessar utilize a URL: http://${HOST_APP}:${PORTA_APP}
+    Ambiente atual: ${process.env.AMBIENTE_PROCESSO}`);
 });
